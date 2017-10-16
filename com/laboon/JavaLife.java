77ff85a4684ccb5f788d2215df6a1e655bc3bbcd
Jake Bader <jsb99@pitt.edu>
2017-10-16 16:00:14 -0400

    actually fixed

--- com/laboon/JavaLife.java
+++ com/laboon/JavaLife.java
@@ -15,7 +15,7 @@ public class JavaLife {
 		System.out.println("Initial Configuration:");
 		System.out.println(w.toString());
 		for (int j=0; j < maxIterations; j++) {
-			w  = w.iterate();
+			w = w.iterate();
 			System.out.println("Iteration " + (j + 1) + ":");
 			System.out.println(w.toString());
 		}
